7da982bd6145eedf5f957647e920438e9ad61292
Andrew Valencik <andrew.valencik@gmail.com>
2015-04-07 12:53:18 -0300

    Updated the schedule with 2015 info

--- js/schedule.js
+++ js/schedule.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-	var may3 = new Date();
-	may3.setFullYear(2014,5,3);
-	var may10 = new Date();
-	may10.setFullYear(2014,5,10);
+	var may9 = new Date();
+	may9.setFullYear(2015,5,9);
+	var may16 = new Date();
+	may16.setFullYear(2015,5,16);
 	var today = new Date();
 	
-	if (may3 > today )
+	if (may9 > today )
 	{
 		$('#collapseOne').addClass('in');
 	} 
-	else if(may3 < today && may10 > today)
+	else if(may9 < today && may16 > today)
 	{
 		$('#collapseTwo').addClass('in');
 	}
